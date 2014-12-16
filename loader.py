28fe46deeaea56e4235d9af271265f6fb91decbe
lonewolf <nikhilkalige@gmail.com>
2014-12-15 20:43:57 -0700

    Added logging and package count logs

--- loader.py
+++ loader.py
@@ -6,13 +6,21 @@ import datetime
 import sys
 import traceback
 import re
+import time
 
 atom_link = "https://atom.io/api/packages?page="
 
 
+
 class Load(Command):
-    def __init__(self, db):
+    def __init__(self, app, db):
+        self.app = app
         self.db = db
+        self.start_time = time.time()
+        self.no_of_updates = 0
+        self.no_of_packages = 0
+        self.new_packages = 0
+        self.skipped_packages = 0
 
     def run(self):
         i = 1
@@ -20,43 +28,61 @@ class Load(Command):
             req = requests.get(atom_link + repr(i))
             i = i + 1
             if (req.status_code != requests.codes.ok) or (req.json() == []):
-                if i is 3:
-                    break
+                end_time = time.time()
+                lapse = end_time - self.start_time
+                self.app.logger.info("\nTotal Pages: %d\n"
+                    "Total Packages: %d\n"
+                    " Updated : %d\n"
+                    " New     : %d\n"
+                    " Skipped : %d\n"
+                    "Run time: %dm %ds\n",
+                    i-1, self.no_of_packages, self.no_of_updates, self.new_packages, self.skipped_packages, lapse/60, lapse%60)
+                break
 
             for value in req.json():
                 name = value.get('name')
+                self.no_of_packages += 1
                 if name is not None and type(name) is dict:
                     name = name.get('name')
 
                 meta = value.get('metadata')
                 if meta is None:
+                    self.skipped_packages += 1
                     continue
 
                 link = meta.get('repository')
                 if link is not None and type(link) is dict:
                     link = link.get('url')
 
-                if meta.get('author') is None and link is not None:
-                    print(link)
-                    m = re.match(r'^.*com?\/(?P<name>.*)/', link)
+                if meta.get('author') is not None:
+                    if type(meta.get('author')) is dict:
+                        author = meta.get('author')
+                        author = author.get('author') or author.get('name')
+                    else:
+                        author = meta.get('author')
+                elif link is not None:
+                    m = re.match(r'^.*com?[:\/](?P<name>.*)/', link)
+                    if m is None:
+                        # no author so skip
+                        self.skipped_packages += 1
+                        self.app.logger.error("Author: %s", link)
+                        continue
                     author = m.groupdict()['name']
-
-                elif type(meta.get('author')) is dict:
-                    author = meta.get('author')
-                    author = author.get('author') or author.get('name')
-
                 else:
-                    author = meta.get('author')
+                    print("author error", name)
 
                 down_no = 0 if value.get('downloads') is None else value.get('downloads')
 
                 query = Package.query.filter_by(name=name).first()
                 if query is None:
+                    self.new_packages += 1
                     query = Package(name=name,
                                     author=author,
                                     link=link,
                                     description=meta.get('description')
                                     )
+                else:
+                    self.no_of_updates += 1
 
                 self.db.session.add(query)
                 downl_model = Downloads(downloads=down_no, package=query)
--- manage.py
+++ manage.py
@@ -4,11 +4,13 @@ from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.sqlalchemy import SQLAlchemy
 import app
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 from loader import Load
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+basedir = os.path.dirname(__file__)
+LOG_FILE = basedir + "/logs/package_loader.log"
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
 
@@ -18,10 +20,17 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
 #migrate = Migrate(app, db)
 
 app_instance = app.create_app("DEVELOPMENT")
+# add logging
+formatter = logging.Formatter('%(asctime)s:  %(message)s')
+handler = RotatingFileHandler(LOG_FILE, maxBytes=512*1024, backupCount=5)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+app_instance.logger.addHandler(handler)
+
 manager = Manager(app_instance)
 manager.add_command("runserver", Server())
 manager.add_command('db', MigrateCommand)
-manager.add_command('load', Load(app.db))
+manager.add_command('load', Load(app_instance, app.db))
 server = Server(host="0.0.0.0", port=9000)
 
 if __name__ == 	"__main__":
