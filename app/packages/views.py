9be1f47eee58332748634691877cf65ae5b6bc3b
lonewolf <nikhilkalige@gmail.com>
2014-12-23 02:18:47 -0600

    Add api for featured

--- app/packages/views.py
+++ app/packages/views.py
@@ -9,12 +9,20 @@ from datetime import timedelta
 def stats():
     resp = dict()
     resp["count"] = Package.get_count()
-    resp["day"] = Downloads.get_downloads_count(timedelta(days=1))
-    resp["week"] = Downloads.get_downloads_count(timedelta(days=7))
-    resp["month"] = Downloads.get_downloads_count(timedelta(days=30))
+    resp["day"] = Downloads.get_overall_downloads_count(timedelta(days=1))
+    resp["week"] = Downloads.get_overall_downloads_count(timedelta(days=7))
+    resp["month"] = Downloads.get_overall_downloads_count(timedelta(days=30))
     return jsonify(resp)
+
 
 @packages.route('/featured', methods=['GET'])
 def featured():
     package_list = requests.get("https://atom.io/api/packages/featured")
     theme_list = requests.get("https://atom.io/api/themes/featured")
+    featured_list = package_list.json() + theme_list.json()
+
+    json_data = []
+    for item in featured_list:
+        json_data.append(Package.get_json(item['name']))
+
+    return jsonify(results=json_data)
