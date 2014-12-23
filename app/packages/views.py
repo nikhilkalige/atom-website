0ef7a313bc9a45629b3369fce5e5f88061e835ae
lonewolf <nikhilkalige@gmail.com>
2014-12-22 23:25:41 -0600

    api for stats created

--- app/packages/views.py
+++ app/packages/views.py
@@ -1,7 +1,20 @@
+import requests
 from . import packages
+from models import Package, Downloads
+from flask import jsonify
+from datetime import timedelta
 
 
-@packages.route('/')
-@packages.route('/index')
-def index():
-    return "Hello, World!"
+@packages.route('/stats', methods=['GET'])
+def stats():
+    resp = dict()
+    resp["count"] = Package.get_count()
+    resp["day"] = Downloads.get_downloads_count(timedelta(days=1))
+    resp["week"] = Downloads.get_downloads_count(timedelta(days=7))
+    resp["month"] = Downloads.get_downloads_count(timedelta(days=30))
+    return jsonify(resp)
+
+@packages.route('/featured', methods=['GET'])
+def featured():
+    package_list = requests.get("https://atom.io/api/packages/featured")
+    theme_list = requests.get("https://atom.io/api/themes/featured")
