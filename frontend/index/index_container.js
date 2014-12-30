029faa093bad184d4485424fc81f37c3a4154071
lonewolf <nikhilkalige@gmail.com>
2014-12-29 23:31:11 -0600

    Working temp commit

--- frontend/index/index_container.js
+++ frontend/index/index_container.js
@@ -2,8 +2,9 @@ var AmpersandView = require("ampersand-view");
 var Template = require("./templates/index.hbs");
 var StatsView = require("./stats_view");
 var FeatureView = require("./feature_view");
-var StatsModel = require("../models/stats")
-//var FeatureCollection = require("../models/featured")
+var StatsModel = require("../models/stats");
+var PackageModel = require("../models/package")
+var FeatureCollection = require("../models/featured")
 
 module.exports = AmpersandView.extend({
     template: Template,
@@ -18,22 +19,24 @@ module.exports = AmpersandView.extend({
                 });
             }
         },
-  /*      featured: {
-            container: "[data-hook=featured]",
-            prepareView: function(el) {
-                this.renderCollection(app.features, FeatureView, this.queryByHook("featured"))
-            }
-        }
-  */  },
+    },
     initialize: function() {
         self = this;
         this.stats_model = new StatsModel();
-        //app.features = new FeatureCollection();
-        this.stats_model.fetch();
-        //app.features.fetch();
+        app.features = new FeatureCollection();
         app.router.on("route:index", function(a, b) {
             console.log("route init");
             self.render();
         });
+        app.features.on("reset", function() {
+            console.log("resetttasdf");
+            self.renderCollection(app.features, FeatureView, self.queryByHook("featured"));
+        })
+        app.router.on("route:package", function(a, b) {
+            console.log(a, b);
+        })
+
+        this.stats_model.fetch();
+        app.features.fetch({reset: true});
     }
 });
