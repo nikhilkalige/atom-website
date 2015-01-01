e938ad668b805a7ee14ec8b5cf0a140d65000157
lonewolf <nikhilkalige@gmail.com>
2015-01-01 15:36:52 -0600

    Prettify data by adding comma for stats

--- frontend/index/stats_view.js
+++ frontend/index/stats_view.js
@@ -5,11 +5,9 @@ var Model = require("../models/stats")
 module.exports = AmpersandView.extend({
     template: Template,
     bindings: {
-        'model.fullName': '[data-hook~=name]',
         "model.count": "[data-hook~=count]",
         "model.day": "[data-hook~=day]",
         "model.month": "[data-hook~=month]",
         "model.week": "[data-hook~=week]",
     }
-    //model: Model
 })
--- frontend/models/stats.js
+++ frontend/models/stats.js
@@ -2,10 +2,22 @@ var AmpersandModel = require("ampersand-model");
 
 module.exports = AmpersandModel.extend({
     props: {
-        count: ["number", true, 0],
-        day: ["number", true, 0],
-        month: ["number", true, 0],
-        week: ["number", true, 0],
+        count: ["string", true, "0"],
+        day: ["string", true, "0"],
+        month: ["string", true, "0"],
+        week: ["string", true, "0"],
+    },
+    parse: function(res) {
+        res.count = this.prettify(res.count);
+        res.day = this.prettify(res.day);
+        res.month = this.prettify(res.month);
+        res.week = this.prettify(res.week);
+        return res;
+    },
+    prettify: function(number) {
+        var parts = number.toString().split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return parts.join(".");
     },
     urlRoot: "api/packages/stats"
 })
