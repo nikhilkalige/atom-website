07b6e567367fa8f07044c23e79c2c6c04e1e6527
lonewolf <nikhilkalige@gmail.com>
2015-01-01 19:51:22 -0600

    Add gitcrypt for encryption

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 import os
+import ConfigParser
+
 
 os.environ['PYTHONINSPECT'] = 'True'
 basedir = os.path.abspath(os.path.dirname(__file__))
+parser = ConfigParser.ConfigParser()
+parser.read("secret_config.cfg")
 
 
 class Default:
     PORT = 8080
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:qaswqasw@localhost/atom-website'
+    API_KEY = parser.get("github", "api_key")
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:' + \
+        parser.get("develop", "db_password").strip('"') + \
+        '@localhost/atom-website-develop'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://root:qaswqasw@localhost/atom-website'
 
 
 class Developement(Default):
@@ -16,7 +24,8 @@ class Developement(Default):
 class Testing(Default):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:qaswqasw@localhost/atom-website-test'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:' + \
+        parser.get("test", "db_password").strip('"') + '@localhost/atom-website-test'
 
 config = {
     'DEFAULT': Default,
