ef9ae6e481bba0ee1dc03caf56cfe323506d9d57
lonewolf <nikhilkalige@gmail.com>
2015-01-03 19:39:29 -0600

    Change graph settings

--- frontend/packages/graph_view.js
+++ frontend/packages/graph_view.js
@@ -14,17 +14,24 @@ module.exports = AmpersandView.extend({
     render: function() {
         MetricsGraphs.convert.date(this.model.downloads, 'date');
         MetricsGraphs.data_graphic({
-            title: "UFO Sightings",
-            description: "Yearly UFO sightings from 1945 to 2010.",
             data: this.model.downloads,
-            width: 650,
+            width: 760,
             height: 250,
             target: this.el,
             show_years: false,
             animate_on_load: true,
             x_accessor: "date",
             y_accessor: "count",
-            interpolate: "monotone"
+            // Axis related
+            min_y_from_data: true,
+            min_x_from_data: true,
+            concise: false,
+            // Layout related
+            buffer: 5,
+            // Graphic
+            interpolate: "linear",
+            point_size: 4,
+            y_rug: true
         });
         console.log("meee");
     }
--- frontend/packages/package_view.js
+++ frontend/packages/package_view.js
@@ -29,7 +29,7 @@ module.exports = AmpersandView.extend({
             type: "text",
             hook: "desc"
         },
-        "model.version.name": {
+        "model.version.number": {
             type: "text",
             hook: "ver"
         },
