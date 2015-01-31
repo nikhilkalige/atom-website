10003b56bee3ef088c1e38fec799592ddac947ab
lonewolf <nikhilkalige@gmail.com>
2015-01-31 01:28:56 -0700

    remove gunicorn logs

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -3,5 +3,5 @@ import multiprocessing
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 proc_name = "atom"
-errorlog = '~/logs/gunicorn.error.log'
-accesslog = '~/logs/gunicorn.access.log'
+# errorlog = '~/logs/gunicorn.error.log'
+# accesslog = '~/logs/gunicorn.access.log'
