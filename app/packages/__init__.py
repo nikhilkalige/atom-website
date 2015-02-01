e0ddd80ea2d23f9b5fc32dd8a5ea13f9cb30da49
lonewolf <nikhilkalige@gmail.com>
2015-02-01 03:43:45 -0700

    Add api for package search based on name and keywords

--- app/packages/__init__.py
+++ app/packages/__init__.py
@@ -18,6 +18,31 @@ def post_get_many(result=None, search_params=None, **kw):
         item.update(item.pop("get_json"))
 
 
+def search_filter(search_params=None, **kw):
+    if (search_params is None) or search_params.get("name") is None:
+        return
+
+    def filter_string(name):
+        filter = []
+        filter.append(dict(name='name',
+                           val='%' + name + '%',
+                           op='like'
+                           )
+                      )
+        filter.append(dict(name="keywords__name",
+                           val=name,
+                           op="any"
+                           ))
+        return filter
+
+    search_params['filters'] = []
+    args = search_params['name'].split()
+    for item in args:
+        search_params['filters'].extend(filter_string(item))
+
+    search_params['disjunction'] = True
+
+
 def api_creator(apimanager):
     apimanager.create_api(models.Package, primary_key='name', methods=['GET'],
                           include_methods=['get_json'],
@@ -26,3 +51,11 @@ def api_creator(apimanager):
         'GET_SINGLE': [post_get_single],
         'GET_MANY': [post_get_many]
     })
+    apimanager.create_api(models.Package, primary_key='name',
+                          collection_name='search',
+                          methods=['GET'],
+                          include_methods=['get_json'],
+                          include_columns=[],
+                          preprocessors={
+                              'GET_MANY': [search_filter]
+                          })
